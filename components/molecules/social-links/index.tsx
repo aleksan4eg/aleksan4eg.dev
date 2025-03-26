c1b68cdc66b2157ac0b84851f99d4b5c187bdc89
Aleksandr Gumroian <aleksandr.gumroian@gmail.com>
2025-03-26 12:31:06 +0100

    Add Mastodon and X Twitter icons to social links

--- components/molecules/social-links/index.tsx
+++ components/molecules/social-links/index.tsx
@@ -1,4 +1,10 @@
-import { github, linkedin, instagram } from "@/components/constants/icons";
+import {
+    github,
+    linkedin,
+    instagram,
+    mastodon,
+    xTwitter,
+} from "@/components/constants/icons";
 import SocialLink from "@/components/molecules/social-links/social-link";
 
 export interface SocialLink {
@@ -17,7 +23,16 @@ const SOCIAL_LINKS = [
         href: "https://www.linkedin.com/in/agumroian",
         icon: linkedin,
     },
-
+    {
+        id: "fosstodon",
+        href: "https://fosstodon.org/@aleksan4eg",
+        icon: mastodon,
+    },
+    {
+        id: "x-twitter",
+        href: "https://x.com/@aleksan4eg",
+        icon: xTwitter,
+    },
     {
         id: "instagram",
         href: "https://www.instagram.com/aleksan4eg/",
