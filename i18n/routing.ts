105163e8eb3270dca973682a53386b9ff504c38a
Aleksandr Gumroian <aleksandr.gumroian@gmail.com>
2025-03-31 00:30:11 +0200

    Add localized pathnames for routing in multiple languages

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,4 +4,27 @@ export const routing = defineRouting({
     locales: ["en", "ru", "cs"],
     defaultLocale: "en",
     localePrefix: "as-needed",
+    pathnames: {
+        "/": "/",
+        "/about": {
+            ru: "/obo-mne",
+            cs: "/o-mne",
+        },
+        "/blog": {
+            ru: "/blog",
+            cs: "/blog",
+        },
+        "/contact": {
+            ru: "/kontakty",
+            cs: "/kontakty",
+        },
+        "/projects": {
+            ru: "/proekty",
+            cs: "/projekty",
+        },
+        "/uses": {
+            ru: "/chto-ispolzuyu",
+            cs: "/co-pouzivam",
+        },
+    },
 });
