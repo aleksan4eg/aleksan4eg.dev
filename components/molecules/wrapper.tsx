a63dcec358c3a4344385e4de434c591678f518cf
Aleksandr Gumroian <aleksandr.gumroian@gmail.com>
2025-04-14 22:45:15 +0200

    refactor: add role attributes to footer, header, and wrapper components for improved accessibility

--- components/molecules/wrapper.tsx
+++ components/molecules/wrapper.tsx
@@ -4,7 +4,10 @@ const Wrapper = ({
     children: React.ReactNode;
 }>) => {
     return (
-        <main className="mx-auto mb-10 flex grow flex-col gap-12 px-3 pt-20 pb-2 md:max-w-[800px] md:pt-24">
+        <main
+            className="mx-auto mb-10 flex grow flex-col gap-12 px-3 pt-20 pb-2 md:max-w-[800px] md:pt-24"
+            role="main"
+        >
             {children}
         </main>
     );
