81cb87a6fae067bda4ded252fdaf2f3c7310ad20
Aleksandr Gumroian <aleksandr.gumroian@gmail.com>
2025-05-08 17:07:59 +0200

    Refactor BookCard component to simplify progress display and update localization

--- components/pages/about/activities/book.tsx
+++ components/pages/about/activities/book.tsx
@@ -15,7 +15,7 @@ export const Book = () => (
         fallback={
             <div className="group border-input relative flex flex-col gap-2.5 rounded-lg border p-3 shadow-xs">
                 <div className="flex gap-3">
-                    <Skeleton className="aspect-2/3 w-14 rounded bg-gray-200 object-cover" />
+                    <Skeleton className="bg-accent aspect-2/3 w-14 rounded object-cover" />
                     <div className="flex grow flex-col justify-between gap-1">
                         <div>
                             <Skeleton className="h-4 w-full rounded" />
