a6f8eb0c34f8d6d3572857286ae1bae93369310f
Aleksandr Gumroian <aleksandr.gumroian@gmail.com>
2025-05-11 15:05:00 +0200

    Add disableTransitionOnChange prop to ThemeProvider for improved performance

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -56,6 +56,7 @@ export default async function RootLayout({
                         defaultTheme="system"
                         enableSystem
                         enableColorScheme
+                        disableTransitionOnChange
                     >
                         <TooltipProvider>
                             <Header />
